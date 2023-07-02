007934e5e1fc4eeb042ebaa2639733caaa28a5fe
Maxime <maxime.durand90@gmail.com>
2023-07-02 07:05:41 +0200

    ajout js

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
 console.log('coucou');
+
+// const photo = document.querySelector('#photo');
+
+// photo.addEventListener('click', function() {
+//     console.log('coucou');
+// })
+
+const photo = document.querySelector('.photo');
+
+photo.addEventListener('click', function () {
+    console.log('object');
+})
